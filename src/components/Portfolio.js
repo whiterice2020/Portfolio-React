4e41e8455c152fed433b25c2b7f0011b0a407916
Paul DelPadre <PDelPadre@rice.local>
2020-08-13 01:12:39 -0400

    Working on portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,233 @@
 import React from "react";
-// import DataArea from "./DataArea";
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+import Button from 'react-bootstrap/Button'
+
 
 function Portfolio () {
     return (
       <>
       <h1>Portfolio</h1>
-       
+      <CardDeck>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Code Refactor</Card.Title>
+      <Card.Text>
+        asdf
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button variant="primary">GitHub Code</Button>
+    <Button variant="primary">Code Refactor Live Page</Button>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>My Portfolio GitHub</Card.Title>
+      <Card.Text>
+      This is my portfolio of all projects and homework assignments created and completed for the duration of the University of Central Florida's Full Stack Developer Boot Camp.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button variant="primary">GitHub Code</Button>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Password Generator</Card.Title>
+      <Card.Text>
+        An application which generates random passwords based on the user's choices.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button variant="primary">GitHub Code</Button>
+    <Button variant="primary">Password Generator Live Page</Button>
+    </Card.Footer>
+  </Card>
+</CardDeck>
+
+{/* New Row of Cards */}
+<br></br>
+<CardDeck>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Javascript Quiz Game</Card.Title>
+      <Card.Text>
+        A fun quiz game about JavaScript!
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button variant="primary">GitHub Code</Button>
+    <Button variant="primary">Javascript Quiz Game Live Page</Button>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Daily Planner GitHub</Card.Title>
+      <Card.Text>
+        A Daily planner which allows you to enter tasks and check them off when completed
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button variant="primary">GitHub Code</Button>
+    <Button variant="primary">Daily Planner Live Page</Button>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Weather Dashboard</Card.Title>
+      <Card.Text>
+        A weather dashboard to display weather for the inputted city
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <Button variant="primary">GitHub Code</Button>
+      <Button variant="primary">Weather Dashboard Live Page</Button>
+    </Card.Footer>
+  </Card>
+</CardDeck>
+
+{/* Next Row of Cards */}
+<br></br>
+<CardDeck>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Electric Gallery</Card.Title>
+      <Card.Text>
+        View art galleries from the Harvard Museum of Art and The Metropolitian Museum
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button variant="primary">GitHub Code</Button>
+    <Button variant="primary">Electric Gallery Live Page</Button>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Good ReadMe Generator</Card.Title>
+      <Card.Text>
+        A Node app which automates the creation of README files for GitHub
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <Button variant="primary">GitHub Code</Button>
+      <Button variant="primary">Good ReadMe Generator Live Page</Button>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Employee Tracker</Card.Title>
+      <Card.Text>
+      An application to architect and build a solution for managing a company's employees
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <Button variant="primary">GitHub Code</Button>
+      <Button variant="primary">Employee Tracker Live Page</Button>
+    </Card.Footer>
+  </Card>
+</CardDeck>
+
+{/* Next Row of Cards */}
+<br></br>
+<CardDeck>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Note Taker</Card.Title>
+      <Card.Text>
+      An application that can be used to write, save, and delete notes.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button variant="primary">GitHub Code</Button>
+    <Button variant="primary">Note Taker Live Page</Button>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Employee Management System</Card.Title>
+      <Card.Text>
+      This application allows you to add employees information and displays them to an HTML Page
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <Button variant="primary">GitHub Code</Button>
+      <Button variant="primary"> Live Page</Button>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Burger Logger</Card.Title>
+      <Card.Text>
+        A fun application which allows you to enter burger orders for dining in or carry out
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <Button variant="primary">GitHub Code</Button>
+      <Button variant="primary">Burger Logger Live Page</Button>
+    </Card.Footer>
+  </Card>
+</CardDeck>
+
+{/* Next Row of Cards */}
+<br></br>
+<CardDeck>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Unheard</Card.Title>
+      <Card.Text>
+        Informative App which displays Drug Overdose Deaths by state.  Users can add their personal stories which will show on the map
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button variant="primary">GitHub Code</Button>
+    <Button variant="primary">Unheard Live Page</Button>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>TBA</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <Button variant="primary">GitHub Code</Button>
+      <Button variant="primary">GitHub Code</Button>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>TBA</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <Button variant="primary">GitHub Code</Button>
+      <Button variant="primary">GitHub Code</Button>
+    </Card.Footer>
+  </Card>
+</CardDeck>
+
       </>
       );
   };
