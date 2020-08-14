6de48bd0ebebeb7399e9cfe4bb38e97df60757e0
Paul DelPadre <PDelPadre@rice.local>
2020-08-13 23:38:21 -0400

    Updates

---
+++ precache-manifest.17c59192677683ba1abdc01a92f6142a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a511a63e760312715a866c090198a3",
+    "url": "/Portfolio-React/index.html"
+  },
+  {
+    "revision": "478e83cf0ec48aceeec8",
+    "url": "/Portfolio-React/static/css/main.b07941b9.chunk.css"
+  },
+  {
+    "revision": "de7d6727e595d465f5d5",
+    "url": "/Portfolio-React/static/js/2.93c9cdef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio-React/static/js/2.93c9cdef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478e83cf0ec48aceeec8",
+    "url": "/Portfolio-React/static/js/main.a193f93e.chunk.js"
+  },
+  {
+    "revision": "2effeafa6a821fc346de",
+    "url": "/Portfolio-React/static/js/runtime-main.5acc9983.js"
+  },
+  {
+    "revision": "aba5fc5beb85a527efa08e79e3110f50",
+    "url": "/Portfolio-React/static/media/Me.aba5fc5b.jpg"
+  },
+  {
+    "revision": "2f1d45caf3548fcfa53bf878c2f233ac",
+    "url": "/Portfolio-React/static/media/paulDelpadreResume.2f1d45ca.pdf"
+  }
+]);
